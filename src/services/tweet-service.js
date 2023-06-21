6c789f0dc127f20855e5fb94e0de4d4366bb31e4
Sachin goyal <sachin@thegeeksquad.tech>
2023-06-21 19:04:11 +0530

    code

--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -9,8 +9,7 @@ class TweetService{
     async create(data){
         const content = data.content;
         let tags = content.match(/#[a-zA-Z0-9_]+/g)
-                        .map((tag) => tag.substring(1)) //regex to get the hashtags and to remove the # from the hashtag
-                        .map(tag => tag.toLowerCase())  //to treat lower and uppercase hashtag as same
+                        .map((tag) => tag.substring(1).toLowerCase()) //regex to get the hashtags and to remove the # from the hashtag and //to treat lower and uppercase hashtag as same
         const tweet = await this.tweetRepository.create(data);
         let alreadyPresentTags = await this.hastagRepository.findByName(tags);  //to check all the hastag already there in db which we are sending in our tweet
         let titleOfPresenttags = alreadyPresentTags.map(tags => tags.title);
