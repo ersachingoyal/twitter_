f2d05eb7bc3b4ea92ecda5bfe2fc6d60135ff7c7
Sachin goyal <sachin@thegeeksquad.tech>
2023-06-21 16:33:27 +0530

    added create service for tweets

--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ const app = express();
 
 const connect = require('./config/database');
 
+const Tweet = require('./models/tweet');
+
 app.listen(3000, async () =>{
     console.log("Server started at port 3000");
     await connect();
     console.log("Mongo db connected");
+
+    const tweet = await Tweet.find({
+        content: ['first tweet']
+    })
+
+    console.log(tweet);
 })
