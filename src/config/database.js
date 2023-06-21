58bd383b028e3d7e07885fc724404852d9e8f291
Sachin goyal <sachin@thegeeksquad.tech>
2023-06-21 18:53:01 +0530

    changed the structure to es modules

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,5 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
-const connect = async() =>{
+export const connect = async() =>{
     await mongoose.connect('mongodb://localhost/twitter_backend'); // this will create a db in mongo named twitter_Dev, but until we add any collections there it wont be visible in the mongo compass
 }
-
-module.exports = connect;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
-const connect = require('./config/database');
+import { connect } from './config/database.js'
+
+import service from './services/tweet-service.js'
  
 app.listen(3000, async () =>{
     console.log("Server started at port 3000");
     await connect();
     console.log("Mongo db connected");
+
+    let ser = new service();
+    ser.create({content: 'Done with #refactor'})
 
 })
